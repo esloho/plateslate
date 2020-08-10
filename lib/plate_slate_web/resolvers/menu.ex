f1b5879c56b89819d9aa16295dc883bed5c39467
Esther Lozano <esloho@gmail.com>
2020-08-10 10:34:12 +0200

    Chapters 1-5 for queries and mutations

---
+++ lib/plate_slate_web/resolvers/menu.ex
@@ -0,0 +1,41 @@
+defmodule PlateSlateWeb.Resolvers.Menu do
+  alias PlateSlate.Menu
+
+  def menu_items(_, args, _) do
+    {:ok, Menu.list_items(args)}
+  end
+
+  def items_for_category(category, _, _) do
+    query = Ecto.assoc(category, :items)
+    {:ok, PlateSlate.Repo.all(query)}
+  end
+
+  def search(_, %{matching: term}, _) do
+    {:ok, Menu.search(term)}
+  end
+
+  def create_item(_, %{input: params}, _) do
+    case Menu.create_item(params) do
+      {:error, changeset} ->
+        {:ok, %{errors: transform_errors(changeset)}}
+      {:ok, menu_item} ->
+        {:ok, %{menu_item: menu_item}}
+    end
+  end
+
+  def transform_errors(changeset) do
+    changeset
+    |> Ecto.Changeset.traverse_errors(&format_error/1)
+    |> Enum.map(fn
+      {key, value} ->
+        %{key: key, message: value}
+    end)
+  end
+
+  @spec format_error(Ecto.Changeset.error) :: String.t
+  defp format_error({msg, opts}) do
+    Enum.reduce(opts, msg, fn {key, value}, acc ->
+      String.replace(acc, "%{#{key}}", to_string(value))
+    end)
+  end
+end
---
+++ test/plate_slate_web/schema/query/menu_items_test.exs
@@ -0,0 +1,172 @@
+defmodule PlateSlateWeb.Schema.Query.MenuItemTest do
+  use PlateSlateWeb.ConnCase, async: true
+
+  setup do
+    PlateSlate.Seeds.run()
+  end
+
+  @query """
+    {
+      menuItems {
+        name
+      }
+    }
+  """
+  test "menuItems field returns menu items" do
+    response = get(build_conn(), "/api", query: @query)
+
+    assert json_response(response, 200) == %{
+      "data" => %{
+          "menuItems" => [
+            %{"name" => "Bánh mì"},
+            %{"name" => "Chocolate Milkshake"},
+            %{"name" => "Croque Monsieur"},
+            %{"name" => "French Fries"},
+            %{"name" => "Lemonade"},
+            %{"name" => "Masala Chai"},
+            %{"name" => "Muffuletta"},
+            %{"name" => "Papadum"},
+            %{"name" => "Pasta Salad"},
+            %{"name" => "Reuben"},
+            %{"name" => "Soft Drink"},
+            %{"name" => "Vada Pav"},
+            %{"name" => "Vanilla Milkshake"},
+            %{"name" => "Water"}
+          ]
+        }
+      }
+  end
+
+  @query """
+    query ($term: String) {
+      menuItems (filter: {name: $term}) {
+        name
+      }
+    }
+  """
+  @variables %{"term" => "reu"}
+  test "menuItems field returns menu items filtered by name" do
+    response = get(build_conn(), "/api", query: @query, variables: @variables)
+
+    assert json_response(response, 200) == %{
+             "data" => %{
+               "menuItems" => [
+                 %{"name" => "Reuben"}
+               ]
+             }
+           }
+  end
+
+  @query """
+    {
+      menuItems (filter: {name: 123}) {
+        name
+      }
+    }
+  """
+  test "menuItems field returns error when using a bad value" do
+    response = get(build_conn(), "/api", query: @query)
+
+    assert %{"errors" => [
+      %{"message" => message}
+    ]} = json_response(response, 400)
+
+    assert message == "Argument \"filter\" has invalid value \{name: 123\}.\nIn field \"name\": Expected type \"String\", found 123."
+  end
+
+  @query """
+    query ($order: SortOrder!){
+      menuItems (order: $order) {
+        name
+      }
+    }
+  """
+  @variables %{"order" => "DESC"}
+  test "menuItems field returns items descending using literals" do
+    response = get(build_conn(), "/api", query: @query, variables: @variables)
+
+    assert %{
+       "data" => %{"menuItems" => [%{"name" => "Water"} | _]}
+   } = json_response(response, 200)
+  end
+
+  @query """
+    query ($filter: MenuItemFilter!){
+      menuItems (filter: $filter) {
+        name
+      }
+  }
+  """
+  @variables %{filter: %{"category" => "Sandwiches", "tag" => "Vegetarian"}}
+  test "menuItems field returns menuItems, filtering with a literal" do
+    response = get(build_conn(), "/api", query: @query, variables: @variables)
+
+    assert %{
+      "data" => %{"menuItems" => [%{"name" => "Vada Pav"}]}
+    } = json_response(response, 200)
+  end
+
+  @query """
+    query ($filter: MenuItemFilter!){
+      menuItems (filter: $filter) {
+        name
+        addedOn
+      }
+  }
+  """
+  @variables %{filter: %{"addedBefore" => "2017-01-20"}}
+  test "menuItems filtered by custom scalar" do
+    sides = PlateSlate.Repo.get_by!(PlateSlate.Menu.Category, name: "Sides")
+    %PlateSlate.Menu.Item{
+      name: "Garlic Fries",
+      added_on: ~D[2017-01-01],
+      price: 2.50,
+      category: sides,
+    } |> PlateSlate.Repo.insert!
+
+    response = get(build_conn(), "/api", query: @query, variables: @variables)
+    assert %{
+             "data" => %{"menuItems" => [%{"name" => "Garlic Fries", "addedOn" => "2017-01-01"}]}
+           } = json_response(response, 200)
+  end
+
+  @query """
+    query ($filter: MenuItemFilter!){
+      menuItems (filter: $filter) {
+        name
+        addedOn
+      }
+  }
+  """
+  @variables %{filter: %{"addedBefore" => "not-a-date"}}
+  test "menuItems filtered by custom scalar with error" do
+    response = get(build_conn(), "/api", query: @query, variables: @variables)
+
+    assert %{"errors" => [%{"locations" => [
+        %{"column" => 0, "line" => 2}], "message" => message}
+    ]} = json_response(response, 400)
+
+    expected = """
+    Argument "filter" has invalid value $filter.
+    In field "AddedBefore": Expected type "Date", found "not-a-date".
+    """
+    assert expected = message
+  end
+
+  @query """
+    query ($filter: MenuItemFilter!){
+      menuItems (filter: $filter) {
+        name
+        price
+      }
+  }
+  """
+  @variables %{filter: %{"priceAbove" => "5"}}
+  test "menuItems filtered by decimal price" do
+    response = get(build_conn(), "/api", query: @query, variables: @variables)
+
+    assert %{"data" => %{"menuItems" => results}} = json_response(response, 200)
+    assert length(results) == 2
+    assert Enum.find(results, &(&1["price"] == "5.5"))
+  end
+end
